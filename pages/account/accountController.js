ddf26fc52c1bc515493646a899d36ac32d81529e
hung <quanghungbui1@gmail.com>
2023-09-27 11:13:09 +0200

    show logoff menu when notAuthorizedUser (only in account page)

--- pages/account/accountController.js
+++ pages/account/accountController.js
@@ -200,7 +200,7 @@
                 },
                 clickTopButton: function (event) {
                     Log.call(Log.l.trace, "Contact.Controller.");
-                    if (AppData.generalData.logOffOptionActive) {
+                    if (AppData.generalData.logOffOptionActive || that.binding.generalData.notAuthorizedUser) {
                         var anchor = document.getElementById("menuButton");
                         var menu = document.getElementById("menu1").winControl;
                         var placement = "bottom";
